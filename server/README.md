dae993bde94a165f221270744ba75d531137952e
riccbru <74079434+riccbru@users.noreply.github.com>
2025-05-20 23:04:59 +0200

    Initialized something

---
+++ server/README.md
@@ -0,0 +1,3 @@
+# Backend API routes
+
+## Baseline URL: `/api/v1`
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import express from "express";
 
 import welcomeRoute from "./routes/hello";
 import usersRoute from "./routes/users";
+import authRoute from "./routes/auth";
 
 dotenv.config();
 const SERVER_PORT = process.env.SERVER_PORT;
@@ -17,7 +18,8 @@ app.use(express.json());
 
 app.use("/api/v1/welcome", welcomeRoute);
 app.use("/api/v1/users", usersRoute);
+app.use("/api/v1/login", authRoute);
 
 app.listen(SERVER_PORT, () => {
-    console.log(`\x1b[42m[*]\x1b[0m \x1b[92mBackend listening on \x1b[1m${SERVER_PORT}...`);
+    console.log(`\x1b[42m[*]\x1b[0m \x1b[92mBackend listening on \x1b[1m${SERVER_PORT}...\x1b[0m`);
 });
